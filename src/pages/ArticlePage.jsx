1f7b32e003345081c267108cfaae918fae4447ab
kunjolee28 <Kunjo.JosueLee@telus.com>
2023-05-04 20:17:13 -0600

    article messages updated

--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -6,12 +6,12 @@ import { modalContext } from '../context/ModalContext';
 import { AppLayout, ModalLayout } from '../layouts/';
 import { SaveArticle, UpdateArticle } from '../components/articles/';
 import { api } from '../api';
+import { swalMessage } from '../helpers';
 
 export const ArticlePage = () => {
     const { handleShow } = useContext(modalContext);
     const [loading, setLoading] = useState(false);
     const [data, setData] = useState([]);
-    const [textResponse, setTextResponse] = useState('');
     const [articleToEdit, setArticleToEdit] = useState(null);
 
     useEffect(() => {
@@ -40,10 +40,16 @@ export const ArticlePage = () => {
                 const { data } = await api.post('/deleteArticle', {
                     idArticulo,
                 });
-                const { response_description } = data;
-                setTextResponse(response_description);
+                const { response_description, response } = data;
+
+                if (response === 0) {
+                    throw new Error('Error!');
+                }
+
+                swalMessage({ text: response_description, title: 'Deleted!' });
             } catch (error) {
-                console.log('Error deleting article');
+                console.log('Error deleting article', error);
+                swalMessage({ text: 'Something went wrong', title: 'Error!', icon: 'error' });
             }
         }
     };
@@ -88,7 +94,6 @@ export const ArticlePage = () => {
                     </tbody>
                 </Table>
             )}
-            {textResponse && <p style={{ fontSize: '32px' }}>{textResponse}</p>}
 
             <Button variant='primary' onClick={() => handleClick()}>
                 Add new article
