760b5b2b3dd9875a6c8693ffd57b7cd803a9ac5a
kunul942 <110417304+kunul942@users.noreply.github.com>
2023-05-31 17:25:54 -0600

    Pago terminado (#6)
    Co-authored-by: DGUGLAS <moratayadouglas800@gmail.com>

---
+++ src/components/Pay/UpdatePay.jsx
@@ -0,0 +1,56 @@
+import { useContext, useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { useForm } from '../../hooks/useForm';
+import { api } from '../../api';
+import { modalContext } from '../../context/ModalContext';
+
+const initialForm = {
+    tipo: '',
+};
+
+export const UpdatePay = ({ IdPago = 0 }) => {
+    const { formState, onInputChange, onResetForm, tipo } = useForm(initialForm);
+    const [loading, setLoading] = useState(false);
+    const [textResponse, setTextResponse] = useState('');
+    const { handleClose } = useContext(modalContext);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+
+        try {
+            const { data } = await api.post('/updateFormaPago', {
+                IdPago,
+                ...fromState,
+            });
+
+            const { response_description } = data;
+
+            setTextResponse(response_description);
+            onResetForm();
+            handleClose();
+        } catch (error) {
+            console.log('Error updating Pay');
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group className='mb-3' controlId='formBasicEmail'>
+                    <Form.Label>Tipo Pago</Form.Label>
+                    <Form.Control type='tipo' name='tipo' onChange={onInputChange} value={tipo} />
+                    <Form.Text className='text-muted'>payment type</Form.Text>
+                </Form.Group>
+
+                {textResponse && <p>{textResponse}</p>}
+                {loading && <p>Loading...</p>}
+                <Button variant='primary' type='submit'>
+                    Submit
+                </Button>
+            </Form>
+        </>
+    );
+};
